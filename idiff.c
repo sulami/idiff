6a2a7369efa3417185494ad687022ec41f1918e0
Robin Schroer <sulami@peerwire.org>
2014-12-08 17:07:01 +0100

    read whole files and allocate space as needed

--- idiff.c
+++ idiff.c
@@ -1,13 +1,14 @@
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define STD_BUFSIZE 128
 
 int
 main(int argc, char *argv[])
 {
-    int **files;
+    char ***files;
     int retval = 0;
 
     if (argc < 3) {
@@ -23,9 +24,17 @@ main(int argc, char *argv[])
 
     for (int i = 0; i < argc - 1; i++) {
         FILE *fp;
+        int bufsize = STD_BUFSIZE;
+        int bufleft = STD_BUFSIZE;
 
-        files[i] = calloc(sizeof(char), STD_BUFSIZE);
+        files[i] = calloc(sizeof(char *), STD_BUFSIZE);
         if (!files[i]) {
+            retval = ENOMEM;
+            goto bufallocfail;
+        }
+
+        *files[i] = malloc(sizeof(char) * STD_BUFSIZE);
+        if (!*files[i]) {
             retval = ENOMEM;
             goto bufallocfail;
         }
@@ -35,14 +44,32 @@ main(int argc, char *argv[])
             retval = EIO;
             goto readfail;
         }
+
+        while (fgets(*files[i], bufsize, fp)) {
+            if (bufleft < 2) {
+                bufsize += STD_BUFSIZE;
+                *files[i] = realloc(*files[i], bufsize);
+                if (!*files[i]) {
+                    retval = ENOMEM;
+                    goto bufallocfail;
+                }
+            }
+            printf("%s", *files[i]);
+            bufleft -= strlen(*files[i]);
+        }
+
         fclose(fp);
     }
 
 readfail:
 bufallocfail:
-    for (int i = 0; i < argc - 1; i++)
-        if (files[i])
-            free((int *)files[i]);
+    for (int i = 0; i < argc - 1; i++) {
+        if (files[i]) {
+            if (*files[i])
+                free(*files[i]);
+            free(files[i]);
+        }
+    }
     free(files);
 out:
     return retval;
