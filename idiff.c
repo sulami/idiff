cfbbc11171bb5ab6fa165dfc58c2a1ce68334900
Robin Schroer <sulami@peerwire.org>
2014-12-09 13:01:45 +0100

    scrap pointer redundancy

--- idiff.c
+++ idiff.c
@@ -8,7 +8,7 @@
 int
 main(int argc, char *argv[])
 {
-    char ***files;
+    char **files;
     int retval = 0;
 
     if (argc < 3) {
@@ -16,7 +16,7 @@ main(int argc, char *argv[])
         goto out;
     }
 
-    files = calloc(argc - 1, sizeof(int *));
+    files = calloc(argc - 1, sizeof(char *));
     if (!files) {
         retval = ENOMEM;
         goto out;
@@ -33,29 +33,23 @@ main(int argc, char *argv[])
             goto bufallocfail;
         }
 
-        *files[i] = malloc(sizeof(char) * STD_BUFSIZE);
-        if (!*files[i]) {
-            retval = ENOMEM;
-            goto bufallocfail;
-        }
-
         fp = fopen(argv[i+1], "r");
         if (!fp) {
             retval = EIO;
             goto readfail;
         }
 
-        while (fgets(*files[i], bufsize, fp)) {
+        while (fgets(files[i], bufsize, fp)) {
             if (bufleft < 2) {
                 bufsize += STD_BUFSIZE;
-                *files[i] = realloc(*files[i], bufsize);
-                if (!*files[i]) {
+                files[i] = realloc(files[i], bufsize);
+                if (!files[i]) {
                     retval = ENOMEM;
                     goto bufallocfail;
                 }
             }
-            printf("%s", *files[i]);
-            bufleft -= strlen(*files[i]);
+            printf("%s", files[i]);
+            bufleft -= strlen(files[i]);
         }
 
         fclose(fp);
@@ -63,13 +57,9 @@ main(int argc, char *argv[])
 
 readfail:
 bufallocfail:
-    for (int i = 0; i < argc - 1; i++) {
-        if (files[i]) {
-            if (*files[i])
-                free(*files[i]);
+    for (int i = 0; i < argc - 1; i++)
+        if (files[i])
             free(files[i]);
-        }
-    }
     free(files);
 out:
     return retval;
